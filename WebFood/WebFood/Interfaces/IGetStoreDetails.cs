52172f421de50f6b3b08d0de3ee67bcbcc73fe04
eranga4u1 <eranga.kdy@gmail.com>
2017-10-05 13:54:13 +0530

    Added interfaces

---
+++ WebFood/WebFood/Interfaces/IGetStoreDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFood.Models;
+
+namespace WebFood.Interfaces
+{
+   public interface IStore
+    {
+        List<Menu> GetSpecialMenuList();
+        List<Menu> GetFullMenuList();
+
+    }
+}
